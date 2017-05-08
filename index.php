9c22486b40a393865590f5be754072144c8ec49b
Shihlin Lu <shihlinlu@gmail.com>
2017-05-08 11:44:44 -0600

    added content, header, and footer styles

--- index.php
+++ index.php
@@ -54,24 +54,36 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-md-6">
-							col-md-6
+							<div class="box">
+								<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+							</div>
 						</div>
 						<div class="col-md-6">
-							col-md-6
+							<div class="box">
+								<p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring</p>
+							</div>
 						</div>
-					</div>
+					</div> <!--/.row-->
 					<div class="row">
 						<div class="col-md-3">
-							col-md-3
+							<div class="circle">
+								<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+							</div>
 						</div>
 						<div class="col-md-3">
-							col-md-3
+							<div class="circle">
+								<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+							</div>
 						</div>
 						<div class="col-md-3">
-							col-md-3
+							<div class="circle">
+								<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+							</div>
 						</div>
 						<div class="col-md-3">
-							col-md-3
+							<div class="circle">
+								<p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+							</div>
 						</div>
 					</div>
 
